d0b410a44c71636c2dc05eba77831c38c49aa7ff
Kansai-Student-Literary-Union <128477261+Kansai-Student-Literary-Union@users.noreply.github.com>
2024-01-01 23:56:31 +0900

    Add files via upload

---
+++ khb2024/battle/team.js
@@ -0,0 +1,10 @@
+var teamname1 = nadaA
+var teamname2 = nadaB
+var teamname3 = rakunan
+var teamname4 = okayamaasahi
+var teamname5 = osakatoin
+var teamname6 = nagata
+
+var teamnamefin1 = nadaA
+var teamnamefin2 = nadaB
+
