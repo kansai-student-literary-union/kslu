34dcde1eac3f6c246e24484f974639f95ce02c36
Kansai-Student-Literary-Union <128477261+Kansai-Student-Literary-Union@users.noreply.github.com>
2024-04-05 16:33:57 +0900

    Delete battle directory

--- battle/B3/teamname.js
+++
@@ -1,4 +0,0 @@
-const redteam = document.getElementById('redteam');
-redteam.textContent =  teamname6[0][0];
-const whiteteam = document.getElementById('whiteteam');
-whiteteam.textContent = teamname4[0][0];
