3c1409d066f33850e2ae5d60c02a1fd17ff6a0bc
関西学生文芸連合-sub <kansai.bungei+web@gmail.com>
2025-08-16 02:17:07 +0900

    Delete khb2024 directory

--- khb2024/battle/B1/teamname.js
+++
@@ -1,4 +0,0 @@
-const redteam = document.getElementById('redteam');
-redteam.textContent =  teamname4[0][0];
-const whiteteam = document.getElementById('whiteteam');
-whiteteam.textContent = teamname5[0][0];
